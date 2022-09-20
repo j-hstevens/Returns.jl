bbb5aef6d86b1353dc6b23bb0e8aa47e75f9f0a0
John Stevens <johnhstevens@proton.me>
2022-09-20 13:19:34 -0400

    Remove load

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,25 @@
 using Returns
 using Test
-using Distributions, Random, LinearAlgebra, CSV, DataFrames, StatsBase
+using Distributions, Random, LinearAlgebra, StatsBase
 
 @testset "Returns.jl" begin
-    #1.a Load Key Files
-    ltcmas = CSV.File("assets/" .* readdir("assets/")) |> DataFrame
-    #1.b Set time(t), simulations(sims),
-    t = 100
-    sims = 1000
-    #1.c Load annual means(means), annual volaility(vols) and correlations
-    means = ltcmas."Arithmetic Return 2022 (%)" ./ 100
-    vols = ltcmas."Annualized Volatility (%)" ./ 100
-    correls = LowerTriangular(Array(ltcmas[:, Between("U.S. Inflation","Gold")]))
-    correls = min.(correls + correls', 1.0)
+    t = 100 #t = time
+    sims = 1000 #sims = simulations
+    means = collect(0.01:0.01:0.1)
+    vols = zeros(length(means)) .+ 0.05
+    correls = zeros(length(means), length(vols)) .+ 0.02
+    for i=1:size(correls,1)
+        correls[i,i] = 1.0
+    end
+
     #2 Create Returns
     returns = CholeskyReturns(t, sims, means, vols, correls)
 
+    #Calculate the means and vols for all each asset classes in all simulations
     allmeans = [[mean(returns[:,i,j]) for i=1:length(means)] for j=1:sims]
     allvols = [[std(returns[:,i,j]) for i=1:length(means)] for j=1:sims]
+
+    #
     rvols = [mean([i[j] for i in allvols]) for j in 1:length(means)]
     rmeans = [mean([i[j] for i in allmeans]) for j in 1:length(means)]
 
